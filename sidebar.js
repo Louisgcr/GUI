fe26ea2cb6b76d31f4a1afe69250e6cd37f078a0
louisgcr <louisgcr@gmail.com>
2016-10-26 23:58:20 +0800

    Just testing

--- sidebar.js
+++ sidebar.js
@@ -3,10 +3,24 @@ function side_open() {
     document.getElementById("mySidenav").style.width = "25%";
     document.getElementById("mySidenav").style.display = "block";
     document.getElementById("openNav").style.display = 'none';
+    resize();
 }
 function side_close() {
     document.getElementById("main").style.marginLeft = "0%";
     document.getElementById("mySidenav").style.display = "none";
     document.getElementById("openNav").style.display = "inline-block";
+    resize();
 }
 
+function resize(){
+    console.log("resized function");
+    var canvas = document.getElementById("FieldCanvas");
+    if (canvas.width  < window.innerWidth)
+    {
+        canvas.width  = window.innerWidth;
+    } 
+    if (canvas.height < window.innerHeight)
+    {
+        canvas.height = window.innerHeight;
+    }
+}
